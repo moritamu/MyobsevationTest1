c111ba5b780875334659a4154ebff7d45be7f5a9
morisum <moris@r.sannet.ne.jp>
2024-10-12 09:12:14 +0900

    Initial Commit

---
+++ MyobsevationTest1/MyobsevationTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyobsevationTest1App.swift
+//  MyobsevationTest1
+//
+//  Created by MsMacM on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct MyobsevationTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyobsevationTest1Tests/MyobsevationTest1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MyobsevationTest1Tests.swift
+//  MyobsevationTest1Tests
+//
+//  Created by MsMacM on 2024/10/12.
+//
+
+import Testing
+@testable import MyobsevationTest1
+
+struct MyobsevationTest1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
